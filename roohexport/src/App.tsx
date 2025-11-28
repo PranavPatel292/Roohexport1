0a510b97c4cde76c6bff3e25fd60c6c3f7062704
Pranav Patel <pranavpatel@Pranavs-MacBook-Pro-2.local>
2025-11-29 01:28:12 +1100

    Add a new home page file and palce bento and hero-carousel components in that file.

--- roohexport/src/App.tsx
+++ roohexport/src/App.tsx
@@ -1,16 +1,9 @@
-import { BentoDemo } from "./components/sections/BentoGrid";
-import HeroCarousel from "./components/sections/HeroCarousel";
-import reactLogo from "./assets/react.svg";
-import { useState } from "react";
-import viteLogo from "/vite.svg";
+import { Home } from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <HeroCarousel />
-      <BentoDemo />
+      <Home />
     </>
   );
 }
---
+++ roohexport/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { BentoDemo } from "@/components/sections/BentoGrid";
+import HeroCarousel from "@/components/sections/HeroCarousel";
+
+export const Home = () => {
+  return (
+    <>
+      <HeroCarousel />
+      <BentoDemo />
+    </>
+  );
+};
