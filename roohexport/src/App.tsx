8d87f73bd77cddf727204dcf13ed2bb36de40909
Pranav Patel <pranavpatel@Pranavs-MacBook-Pro-2.local>
2025-11-29 02:37:10 +1100

    Netlify deployment with About Us page.

--- roohexport/src/App.tsx
+++ roohexport/src/App.tsx
@@ -1,7 +1,4 @@
 import AboutPage from "./pages/About_Us_Page";
-import ContactPage from "./pages/Contact_Page";
-import { Home } from "./pages/Home";
-import ProductListingPage from "./pages/ProductsList";
 
 function App() {
   return (
